821d300509590eb862c807a4dc9ff7c0fffc70e8
GAVARD Ewann <gavard.e@gmail.com>
2017-05-25 15:41:30 +0200

    Update exec.php

--- exec.php
+++ exec.php
@@ -4,9 +4,9 @@
   if(isset($trigger)){
     if($trigger == 'up'){
     resetAll();
-      exec("gpio mode 15 out");
+      exec("gpio mode 4 out");
       sleep(23);
-	  exec("gpio mode 15 in");
+	  exec("gpio mode 4 in");
     }elseif ($trigger == 'down') {
     resetAll();
       exec("gpio mode 1 out");
@@ -19,6 +19,6 @@
   
   function resetAll(){
     exec("gpio mode 1 in");
-	  exec("gpio mode 15 in");
+	  exec("gpio mode 4 in");
   }
 ?>
