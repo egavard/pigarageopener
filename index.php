1696f38ea41574ed242bdafb60e0ed105e4371db
GAVARD Ewann <gavard.e@gmail.com>
2017-06-14 18:01:11 +0200

    Protocol free urls

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="//fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <!--Import materialize.css-->
   <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
 
@@ -67,7 +67,7 @@
   </div>
 
 </body>
-<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="//code.jquery.com/jquery-2.1.1.min.js"></script>
 <script type="text/javascript" src="js/materialize.min.js"></script>
 <script type="text/javascript">
 $(document).ready(function(){
